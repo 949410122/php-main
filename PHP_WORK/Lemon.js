b111c5fdb108ef0055623673894613442981ea7d
949410122 <46402526+949410122@users.noreply.github.com>
2021-07-14 12:08:34 +0800

    Delete PHP_WORK directory

--- PHP_WORK/Lemon.js
+++
@@ -1,8 +0,0 @@
-function customer(){
-    window.location.href="/customer/home.html";
-    window.event.returnValue=false;
-}
-function staff(){
-    window.location.href="/staff/home.html";
-    window.event.returnValue=false;
-}
--- PHP_WORK/php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $user = "root";
-    $pwd = "";
-    $db = "";
-    $conn = mysqli_connect($hostname, $user ,$pwd ,$db)
-        or die(mysqli_connect_error());
-?>
